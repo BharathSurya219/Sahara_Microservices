a93b9d961482f93ef4d24feea2dfcb0b7fce5b20
bhasu <bhasu@LAPTOP-R36P3QEB.mshome.net>
2024-07-14 17:10:14 +0530

    redis config

--- gateway/src/main/java/com/sahara/gateway/config/RedisConfiguration.java
+++ gateway/src/main/java/com/sahara/gateway/config/RedisConfiguration.java
@@ -2,21 +2,20 @@ package com.sahara.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfiguration {
 
-	 @Bean
-	    JedisConnectionFactory jedisConnectionFactory() {
-	        return new JedisConnectionFactory();
-	    }
 	 
 	 @Bean
-	    public RedisTemplate<String, Object> redisTemplate() {
-	        RedisTemplate<String, Object> template = new RedisTemplate<>();
-	        template.setConnectionFactory(jedisConnectionFactory());
+	    public RedisTemplate redisTemplate(RedisConnectionFactory factory) {
+	        RedisTemplate template = new RedisTemplate<>();
+	        template.setConnectionFactory(factory);
+	        template.setKeySerializer(new StringRedisSerializer());
+	        template.setValueSerializer(new StringRedisSerializer());
 	        return template;
 	    }
 }
