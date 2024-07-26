931c59da338001b038569e3ba21ec651d06abd75
bhasu <bhasu@LAPTOP-R36P3QEB.mshome.net>
2024-07-26 21:36:20 +0530

    product suggestions service

---
+++ product-suggestions/src/main/java/com/sahara/product_suggestions/ProductSuggestionsApplication.java
@@ -0,0 +1,13 @@
+package com.sahara.product_suggestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSuggestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSuggestionsApplication.class, args);
+	}
+
+}
