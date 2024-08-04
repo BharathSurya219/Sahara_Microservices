23e5c9e3d1b073170765e376c6bb02b4b626f833
bhasu <bhasu@LAPTOP-R36P3QEB.mshome.net>
2024-08-04 22:18:34 +0530

    testing sample route with gateway

--- user/src/main/java/com/sahara/user/Controller/SampleAPI.java
+++ user/src/main/java/com/sahara/user/Controller/SampleAPI.java
@@ -1,4 +1,4 @@
-package controller;
+package com.sahara.user.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class SampleAPI {
 
-	@GetMapping(name = "/ok",value = "/ok")
+	@GetMapping(name = "/ok", value = "/ok")
 	public String name() {
-		return "OK";
+		return "Ok ra";
 	}
 }
